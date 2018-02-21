4928eb68d21b717b203e7714ecde21fc72ab62ac
Torsten Friebe <torsten.friebe@gmail.com>
2018-02-21 19:14:05 +0100

    Fixed import for EntityScan as of Spring boot 1.4 the package is org.springframework.boot.autoconfigure.domain.EntityScan

--- demo/SpringDataRestRepository/src/main/java/net/gfu/seminar/spring/data/rest/Application.java
+++ demo/SpringDataRestRepository/src/main/java/net/gfu/seminar/spring/data/rest/Application.java
@@ -2,16 +2,12 @@ package net.gfu.seminar.spring.data.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-// TODO for boot 1.4+ import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 
-/**
- * Created by tf on 27.11.14.
- */
 @Configuration
 @EnableJpaRepositories
 @Import({RepositoryRestMvcConfiguration.class, DataSourceConfig.class})
